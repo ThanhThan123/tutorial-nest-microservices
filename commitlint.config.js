621e39e1770d1a36cc46b8bfe8a63c888689cb1e
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-01-20 15:50:32 +0700

    chore/#commitLint: custom configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,45 @@
 module.exports = {
+  // Kế thừa rules cơ bản từ bộ quy tắc Conventional Commits
   extends: ['@commitlint/config-conventional'],
+
+  // Custom parser để định nghĩa format commit header
+  parserPreset: {
+    parserOpts: {
+      // Regex parse commit message
+      // Format: <type>#<scope>: <subject>
+      // Ví dụ: feat#auth: thêm chức năng đăng nhập với JWT
+      headerPattern: /^(\w+)#(\w+): (.*)$/,
+
+      // Ánh xạ các group trong regex vào field của commitlint
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+
+  // Các rules để enforce commit message
+  rules: {
+    // Chỉ cho phép type thuộc danh sách này
+    // feat = thêm chức năng, fix = sửa bug, docs = tài liệu,
+    // style = format code, refactor = tối ưu, test = test code,
+    // chore = việc vặt, revert = rollback commit
+    'type-enum': [
+      2, // level 2 = error (reject commit nếu sai)
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
+    ],
+
+    // Header (toàn bộ commit message dòng đầu) tối thiểu 10 ký tự
+    'header-min-length': [2, 'always', 10],
+
+    // Header tối đa 160 ký tự
+    'header-max-length': [2, 'always', 160],
+
+    'body-max-line-length': [2, 'always', 120],
+
+    // Mo ta theo nhieu dinh dang style
+    'subject-case': [
+      0,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
 };
