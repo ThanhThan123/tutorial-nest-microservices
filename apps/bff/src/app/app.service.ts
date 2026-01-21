d2553157855594a4933acf523c2e5b4047b6d35a
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-01-21 16:58:10 +0700

    chore/#config: build ResponseDto to standardization API Response

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    throw new BadGatewayException('Bad GetWay');
     return { message: 'Hello API' };
   }
 }
