047be3c937961b03c18a5a4672e4899d56b34389
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-01-27 18:22:48 +0700

    feat/#UserAccessServiceVRoleModule: Initial User Access and build RoleModule

---
+++ libs/constants/src/lib/enum/role.enum.ts
@@ -0,0 +1,13 @@
+export enum ROLE {
+  ADMINISTRATOR = 'administrator',
+  ACCOUNTANT = 'accountant',
+}
+
+export enum PERMISSION {
+  INVOICE_CREATE = 'invoice.create',
+  INVOICE_GET_BY_ID = 'invoice.get_by_id',
+  INVOICE_GET_ALL = 'invoice.get_all',
+  INVOICE_UPDATE = 'invoice.update',
+  INVOCIE_DELETE = 'invoice.delete',
+  INVOICE_SEND = 'invoice.send',
+}
