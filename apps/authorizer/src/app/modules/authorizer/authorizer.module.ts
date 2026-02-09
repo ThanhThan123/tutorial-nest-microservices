c512cb10e354a8f7563024d7567843f89cee1647
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-02-09 16:39:13 +0700

    feat/#CommunicationBetween2Service: Communication gRPC between 2 service

--- apps/authorizer/src/app/modules/authorizer/authorizer.module.ts
+++ apps/authorizer/src/app/modules/authorizer/authorizer.module.ts
@@ -4,10 +4,11 @@ import { AuthorizerController } from './controllers/authorizer.controller';
 import { KeycloakModule } from '../keycloak.module';
 import { TCP_SERVICES, TcpProvider } from '@common/configuration/tcp.config';
 import { ClientsModule } from '@nestjs/microservices';
+import { AuthorizerGrpcController } from './controllers/authorizer-grpc.controller';
 
 @Module({
   imports: [KeycloakModule, ClientsModule.registerAsync([TcpProvider(TCP_SERVICES.USER_ACCESS_SERVICE)])],
-  controllers: [AuthorizerController],
+  controllers: [AuthorizerController, AuthorizerGrpcController],
   providers: [AuthorizerService],
 })
 export class AuthorizerModule {}
--- libs/guards/src/lib/user.guard.ts
+++ libs/guards/src/lib/user.guard.ts
@@ -3,22 +3,28 @@ import { firstValueFrom, map, Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
 import { MetadataKeys } from '@common/constants/common.constant';
 import { getAccessToken, setUserData } from '@common/utils/request.util';
-import { TcpClient } from '@common/interfaces/tcp/common/tcp-client.interface';
-import { TCP_SERVICES } from '@common/configuration/tcp.config';
-import { TCP_REQUEST_MESSSAGE } from '@common/constants/enum/tcp-request-message.enum';
 import { AuthorizeResponse } from '@common/interfaces/tcp/authorizer';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { createHash } from 'crypto';
+import { GRPC_SERVICES } from '@common/configuration/grpc.config';
+import { ClientGrpc } from '@nestjs/microservices';
+import { AuthorizerService } from '@common/interfaces/grpc/authorizer';
 @Injectable()
 export class UserGuard implements CanActivate {
   private readonly logger = new Logger(UserGuard.name);
+  private authorizerService: AuthorizerService;
 
   constructor(
     private readonly reflector: Reflector,
-    @Inject(TCP_SERVICES.AUTHORIZER_SERVICE) private readonly authorizerClient: TcpClient,
+
     @Inject(CACHE_MANAGER) private cacheManage: Cache,
+    @Inject(GRPC_SERVICES.AUTHORIZER_SERVICE) private readonly grpcAuthorizerClient: ClientGrpc,
   ) {}
+
+  onModuleInit() {
+    this.authorizerService = this.grpcAuthorizerClient.getService<AuthorizerService>('AuthorizerService');
+  }
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const authOptions = this.reflector.get<{ secured: boolean }>(MetadataKeys.SECURED, context.getHandler());
@@ -44,13 +50,18 @@ export class UserGuard implements CanActivate {
         return true;
       }
 
-      const result = await this.verifyUserToken(token, processId);
+      const response = await firstValueFrom(this.authorizerService.verifyUserToken({ processId, token }));
+
+      const { data: result } = response;
       if (!result?.valid) {
         throw new UnauthorizedException('Token not valid');
       }
       this.logger.debug(` Set user data to cache for cache key: ${cacheKey}`);
 
       setUserData(req, result);
+      if (!result?.valid) {
+        throw new UnauthorizedException('Token not valid');
+      }
       this.cacheManage.set(cacheKey, result, 30 * 60 * 1000);
       return true;
     } catch (error: any) {
@@ -60,16 +71,6 @@ export class UserGuard implements CanActivate {
       if (error instanceof UnauthorizedException) throw error;
       throw new UnauthorizedException('Token is invalid');
     }
-  }
-  private async verifyUserToken(token: string, processId: string) {
-    return firstValueFrom(
-      this.authorizerClient
-        .send<AuthorizeResponse, string>(TCP_REQUEST_MESSSAGE.AUTHORIZER.VERIFY_USER_TOKEN, {
-          data: token,
-          processId,
-        })
-        .pipe(map((data) => data.data)),
-    );
   }
 
   generateTokenCacheKey(token: string): string {
