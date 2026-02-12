57406d94865a4640cea3239c6e15d3833e9d4ec2
ThanhThan123 <nguyenthanhthan244@gmail.com>
2026-02-12 13:00:41 +0700

    feat/#InitialMailSWithKafka: Initial Mail Service With Kafka Consummer

---
+++ apps/mail/src/configuration/index.ts
@@ -0,0 +1,18 @@
+import { BaseConfiguration } from '@common/configuration/base.config';
+import { AppConfiguration } from '@common/configuration/app.config';
+import { ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Kafka } from '@nestjs/microservices/external/kafka.interface';
+import { KafkaConfiguration } from '@common/configuration/kafka.config';
+class Configuration extends BaseConfiguration {
+  @ValidateNested()
+  @Type(() => AppConfiguration)
+  APP_CONFIG = new AppConfiguration();
+
+  @ValidateNested()
+  @Type(() => KafkaConfiguration)
+  KAFKA_CONFIG = new KafkaConfiguration();
+}
+export const CONFIGURATION = new Configuration();
+
+export type TConfiguration = typeof CONFIGURATION;
